4ea3c5e659d4486e822cfa048adae437764c111a
Leonardo Padilha <lcfpadilha@gmail.com>
2016-11-03 22:58:08 -0200

    ajustes no inicio do simulador

--- EP3.java
+++ EP3.java
@@ -47,10 +47,11 @@ public class EP3 {
          // Enquanto houver processos no arquivo de trace
          while (in.hasNextLine()) {
             line = in.nextLine();
+
             // Leitura dos dados do processo
             s = new Scanner(line);
             int      t0 = s.nextInt();
-            String name = s.next();
+            String name = s.next(); //TODO: Ajustar o nome
             int      tf = s.nextInt();
             int       b = s.nextInt();
 
@@ -72,30 +73,97 @@ public class EP3 {
       }
    } 
 
+   public static int[] select_event (int tmin) {
+      int[] event = new int[2];
+
+      if (!next_process.isEmpty()) {
+         Process p = next_process.element();
+         tmin = p.t0;
+         event[1] = 1;
+      }
+
+      if (!next_pages.isEmpty()) {
+         if (tmin == -1 || next_pages.element().t_access < tmin) {
+            Page p = next_pages.element();
+            tmin = p.t_access;
+            event[1] = 2;
+         }
+      }
+
+      if (!finished_process.isEmpty()) {
+         if (tmin == -1 || finished_process.element().tf < tmin) {
+            Process p = finished_process.element(); 
+            tmin = p.tf;
+            event[1] = 3;
+         }
+      }
+      event[0] = tmin;
+      
+      return event;
+   }
+
    public static void init_simulator () {
-      //Enquanto houver paginas/processos nas listas
-      //Se um processo tem o tempo para 
+      int time = 0;
+
+      while (!next_process.isEmpty() || !next_pages.isEmpty() || !finished_process.isEmpty()) {
+         int tmin = -1;
+         int event[];
+
+         // Seleciona o próximo evento (aquele com tempo menor)
+         event = select_event (tmin);
+         tmin = event[0];
+
+         while (tmin != -1 && tmin < time + interval) {
+            if (event[1] == 1) {
+               Process proc_next = next_process.removeFirst();
+               // Aloca espaço livre pra ele
+               while (!proc_next.pages.isEmpty())
+                  next_pages.add(proc_next.pages.removeFirst());
+            }
+            else if (event[1] == 2) {
+               Page p = next_pages.removeFirst();
+               // Acessa a pagina
+
+            }
+            else if (event[1] == 3) // Remove o espaco que ele ocupou;
+            
+            event = select_event (-1);
+            tmin = event[0];
+            System.out.println (tmin + " " + time + " " + interval);
+         }
+         time += interval;
+      }
    }
 
    public static void execute_command (String[] s) {
       //Carrega arquivo de trace no simulador
-      if (s[0].equals("carrega"))
+      if (s[0].equals("carrega")) {
          set_traceFile(s[1]);
+         System.out.println ("console: trace carregado!");
+      }
       //Define o algoritmo de alocação
-      else if (s[0].equals("espaco"))
+      else if (s[0].equals("espaco")) {
          alg_space = Integer.parseInt(s[1]);
+         System.out.println ("console: algoritmo de gerencia de espaco livre numero " + alg_space + " selecionado!");
          //sim.set_allocManagement(s[1]);
+      }
       //Define o algoritmo de paginação
-      else if (s[0].equals("substitui")) 
+      else if (s[0].equals("substitui")) {
          alg_pages = Integer.parseInt(s[1]);
+         System.out.println ("console: algoritmo de paginação numero " + alg_space + " selecionado!");
          //sim.set_pageManagement(s[1]);
+      }
       //Intervalo de tempo
-      else if (s[0].equals("intevalo"))
+      else if (s[0].equals("intervalo")) {
          interval = Double.parseDouble(s[1]);
+         System.out.println ("console: intervalo selecionado!");
+      }
       //Inicia simulador
-      else if (s[0].equals("executa"))
+      else if (s[0].equals("executa")) {
+         System.out.println ("console: executando o simulador!");
          init_simulator();
          //sim.init();
+      }
       else {
          System.out.println ("Comando INVÁLIDO!");
       }
@@ -111,12 +179,12 @@ public class EP3 {
          if (!in.hasNextLine()) break;
          
          // Lemos os argumentos da linha
-         com_args = in.nextLine().split(" ");
+         com_args = in.nextLine().split("\\s+");
 
          // Executamos o comando passado
          if (com_args.length > 0 && com_args[0].equals("sai"))
             break;
-         else if (com_args.length == 2)
+         else if (com_args.length > 0)
            execute_command(com_args);
       }
    }
--- Page.java
+++ Page.java
@@ -4,7 +4,7 @@ public class Page {
    int t_access;
    int page;
 
-   public Page (int t_access, int page) {
+   public Page (int page, int t_access) {
       this.t_access = t_access;
       this.page     = page;
    }
